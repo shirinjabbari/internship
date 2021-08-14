3347f36f54f2dc3aa25d4b6759cc9d6fd57af5ee
DESKTOP-DBPUL65\Shirin <shirinjabbari799@gmail.com>
2021-08-14 16:29:49 +0430

    cmmit3

--- app.js
+++ app.js
@@ -1,56 +1,84 @@
-
-//window.onload = function(){
-    //var http = new XMLHttpRequest();
-    //http.onreadystatechange = function(){
-       // if(http.readyState == 4 && http.status== 200){
-            //console.log(http.response);
-        //}
-        
-    //};
-    //http.open("GET","https://jsonplaceholder.typicode.com/posts",true);
-    //http.send();
-    //jQUERY METHOD
-   // $.get("https://jsonplaceholder.typicode.com/posts", (data) => {
-    // console.log(data);
-    //});
-    
-//};
-
 const infoBtn = document.getElementById('btn');
-const divElement = document.getElementById('persons-info');
 const tBodyElement = document.getElementById('data');
-const xTable=document.getElementById('text-table');
-const body = xTable.tBodies[0];
-const rows = body.rows;
 let stringOfInfo = "";
 const createRequest = () =>{
     let ourRequest = new XMLHttpRequest();
     ourRequest.open('GET','https://jsonplaceholder.typicode.com/posts');
     ourRequest.onload = () =>{
-        let ourData = JSON.parse(ourRequest.responseText);
+        let ourData = JSON.parse(ourRequest.responseText).slice(0,10);
         renderHTML(ourData);
+        clearString();
     };
     ourRequest.send();
 }
 const renderHTML = (data) => {
     for(let i = 0 ; i< data.length ; i++) {
-     // stringOfInfo = "<p>" + "UserId: " + data[i].userId + "<br>" + "id: "+ data[i].id+"<br>" + "title: "+data[i].title+"<br>" + "body: " + data[i].body + "<p/>";
-     // divElement.insertAdjacentHTML('beforeend',stringOfInfo);
-        stringOfInfo +="<tr class = 'row-j'>";
-        //stringOfInfo +="<td>"+data[i].userId+"</td>";
+        stringOfInfo +="<tr>";
         stringOfInfo +="<td>"+data[i].id+"</td>";
         stringOfInfo +="<td>"+data[i].title+"</td>";
         stringOfInfo +="<td>"+data[i].body+"</td><tr/>";
-        tBodyElement.innerHTML=stringOfInfo;
-        if(i < 19){
-            continue;
-        }else{
-            break;
-        }     
-     } 
-    rows[rows.length - 1].insertAdjacentHTML('beforebegin', "<tr><td colspan=3><input type='text' name='id-box' placeholder='id'><input type='text' name='topic-box' placeholder='topic'><input type='text' name='text' placeholder='text'></td></tr>") ;  
+        tBodyElement.innerHTML=stringOfInfo;     
+     }  
 };
+ const clearString = () =>{
+   stringOfInfo = "";
+   return stringOfInfo 
+ };
+ const searchTopic= ()=>{
+  // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInputTopic");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td= tr[i].getElementsByTagName("td")[1];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+ }
+ const searchText = ()=>{
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInputBody");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+      td= tr[i].getElementsByTagName("td")[2];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+ }
+ const searchId = () =>{
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInputId");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+      td= tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+ }
 infoBtn.addEventListener('click', createRequest);
-
 
  
